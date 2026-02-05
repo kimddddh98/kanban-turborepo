b7e572685306c47917ee5bde50e13b7e43d124a3
kimddddh98 <amawang11@gmail.com>
2026-02-05 23:53:09 +0900

    refactor : Button.stories 네이밍 변경

--- packages/ui/src/components/Button.stories.tsx
+++ packages/ui/src/components/Button.stories.tsx
@@ -5,6 +5,7 @@ import { Button } from './Button';
 
 const meta = {
   component: Button,
+  // 디폴트 args
   args: {
     text: '기본 텍스트',
     onClick: () => alert('버튼 클릭'),
@@ -23,9 +24,16 @@ const ButtonWithHooks = () => {
   return <Button onClick={handleOnChange} text={value} />;
 };
 
-export const Default: Story = {};
-
+export const Default: Story = {
+  name: '기본 버튼',
+};
+export const Secondary: Story = {
+  name: '두번째 버튼',
+  args: {
+    text: '두번째 버튼',
+  },
+};
 export const Primary = {
-  name: '버튼',
+  name: '리액트 훅과 같이 쓰는 버튼',
   render: () => <ButtonWithHooks />,
 } satisfies Story;
