1042a040ec1eb3f69b05edbd0d54e4976eaf3db2
kimddddh98 <amawang11@gmail.com>
2026-02-09 01:40:07 +0900

    test: 환경변수 추가 및 ecs 배포 테스트

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <Button text="테스트" onClick={() => alert('테스트')} />
+      <p>API 주소: {process.env.NEXT_PUBLIC_API_URL}</p>
     </div>
   );
 }
