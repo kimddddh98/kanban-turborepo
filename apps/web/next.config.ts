8ee98f84efa0c08c76ceb622d026c03028ef743b
kimddddh98 <amawang11@gmail.com>
2026-02-08 18:00:38 +0900

    chore: @repo/web - nextjs 도커 이미지 생성

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   transpilePackages: ['@repo/ui', '@repo/utils'],
+  output: 'standalone', // Docker 환경 필수 설정
 };
 
 export default nextConfig;
