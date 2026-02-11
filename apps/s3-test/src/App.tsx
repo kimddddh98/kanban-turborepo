20bc200868ee41499b86b3adffd199278270799c
kimddddh98 <amawang11@gmail.com>
2026-02-11 23:12:21 +0900

    chore : @repo/s3-test - @repo/ui 의존성 추가 및 테스트용 코드 추가

--- apps/s3-test/src/App.tsx
+++ apps/s3-test/src/App.tsx
@@ -1,11 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import { Button } from '@repo/ui';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
@@ -18,18 +16,17 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <Button text="react/vite 버튼" onClick={() => alert('테스트')} />
+        <Button
+          text="github actions"
+          onClick={() => alert('github actions!')}
+        />
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
