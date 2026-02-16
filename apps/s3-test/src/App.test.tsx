a6587ee20f56cdc153e94898cb6ad10e02a16056
kimddddh98 <amawang11@gmail.com>
2026-02-16 18:37:01 +0900

    test: pr 실패 테스트 원복

--- apps/s3-test/src/App.test.tsx
+++ apps/s3-test/src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 describe('App', () => {
   it('renders hello world', () => {
     render(<App />);
-    expect(screen.getByText('Hello Worl')).toBeInTheDocument();
+    expect(screen.getByText('Hello World')).toBeInTheDocument();
   });
 });
