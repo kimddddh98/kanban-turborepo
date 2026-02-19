a34d73ba8a2dd4e0f126dcd68a7b8753dd448ffd
kimddddh98 <amawang11@gmail.com>
2026-02-20 00:37:09 +0900

    test: @repo/s3-test - 도메인,인증서 등록 후 배포 테스트

--- apps/s3-test/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import App from './App';
-import { render, screen } from '@testing-library/react';
-
-describe('앱 테스트', () => {
-  it('renders hello world', () => {
-    render(<App />);
-    expect(screen.getByText('Hello World')).toBeInTheDocument();
-  });
-});
